d2d49f5ef63bfbab5c8d34bd0fd5ace1fe8d950c
musa khosa <39824359+musa-leaf@users.noreply.github.com>
2024-11-13 21:49:26 +0200

    fix: move const strings to utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const SOURCE_DESTINATION_ERROR_MSG = 'Source and destination must not be null';
+export const ENDLINE = '\n';
+export const BUFFER_SIZE_ERROR = 'Buffer size must be greater than zero';
